0fe489866fd4fbbe38aaba047f0a47efa84e0f18
Rokon007 <mdrokonuzzaman099@gmail.com>
2026-03-24 10:26:16 +0600

    2nd commit

--- A1.cpp
+++ A1.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n;
+    cout << "Enter the size of an array:";
+    cin>>n;
+    cout << "Enter " <<n <<" number of array:";
+
+    int arr[n];
+    for (int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+  for (int i=0; i<n; i++)
+  {
+      cout <<arr[i] << " ";
+
+  }
+  return 0;
+
+
+
+
+
+
+}
+
+
 
