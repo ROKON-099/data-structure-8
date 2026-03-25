7f031612294dfacf7b609b8b135ad8b133ac322c
Rokon007 <mdrokonuzzaman099@gmail.com>
2026-03-25 12:35:36 +0600

    added store.cpp

--- Store.cpp
+++ Store.cpp
@@ -1,22 +1,26 @@
 #include<iostream>
 using namespace std;
-int main (){
+int main ()
+{
     int n;
     cout << "Enter the size of an array:";
     cin>>n;
     cout << "Enter " <<n <<" number of array:";
-
     int arr[n];
     for (int i=0; i<n; i++)
     {
         cin>>arr[i];
-    }
-  for (int i=0; i<n; i++)
-  {
-      cout <<arr[i] << " ";
 
-  }
-  return 0;
+    }
+    for (int i=0; i<n ; i++)
+    {
+        cout << arr[i] << " ";
+
+    }
+    return 0;
+
+
+
 
 
 
@@ -24,6 +28,5 @@ int main (){
 
 
 }
-
 
 
